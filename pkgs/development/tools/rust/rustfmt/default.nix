ddf767d2cf60cd5fd08aa5a0e5434a788d68430f
Vladyslav M <dywedir@gra.red>
2019-07-05 18:28:16 +0300

    rustfmt: 1.2.2 -> 1.3.1

--- pkgs/development/tools/rust/rustfmt/default.nix
+++ pkgs/development/tools/rust/rustfmt/default.nix
@@ -1,17 +1,17 @@
 { stdenv, fetchFromGitHub, rustPlatform, darwin }:
 
 rustPlatform.buildRustPackage rec {
-  name = "rustfmt-${version}";
-  version = "1.2.2";
+  pname = "rustfmt";
+  version = "1.3.1";
 
   src = fetchFromGitHub {
     owner = "rust-lang";
-    repo = "rustfmt";
+    repo = pname;
     rev = "v${version}";
-    sha256 = "1k9p6sp8q87flx9vzg46880ir7likvbydai3g6q76278h86rn0v8";
+    sha256 = "1xw2xz2p5x3nm3169fl8l5b7gf4nxagz59fax46jsrfh80m4cjna";
   };
 
-  cargoSha256 = "08x6vy5v2vgrk3gsw3qcvv52a7hifsgcsnsg1phlk1ikaff21y4z";
+  cargoSha256 = "0z7xg22ssk3pmb96r1v1aq0bcig9xaw4ks0j8na833xi73zk2m94";
 
   buildInputs = stdenv.lib.optional stdenv.isDarwin darwin.apple_sdk.frameworks.Security;
 
@@ -26,7 +26,7 @@ rustPlatform.buildRustPackage rec {
 
   meta = with stdenv.lib; {
     description = "A tool for formatting Rust code according to style guidelines";
-    homepage = https://github.com/rust-lang-nursery/rustfmt;
+    homepage = https://github.com/rust-lang/rustfmt;
     license = with licenses; [ mit asl20 ];
     maintainers = [ maintainers.globin ];
     platforms = platforms.all;
